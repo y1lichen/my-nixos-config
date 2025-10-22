c5465298fc121c0abfa884e305c0ff699cf76183
y1lichen <f1jefferson2@gmail.com>
2025-10-23 01:25:36 +0800

    fix hardware

--- modules/hardware/vm.nix
+++ modules/hardware/vm.nix
@@ -1,11 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  fileSystems."/".device = "/dev/vda1";
+  fileSystems."/".device = "/dev/vda2";
   fileSystems."/".fsType = "ext4";
+
+  fileSystems."/boot/efi" = {
+    device = "/dev/vda1";
+    fsType = "vfat";
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   networking.hostName = "laptop";
 }
