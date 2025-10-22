44cf97ab495971feea46248a98f546fba139684a
y1lichen <f1jefferson2@gmail.com>
2025-10-23 01:30:30 +0800

    fix hardware

--- modules/hardware/vm.nix
+++ modules/hardware/vm.nix
@@ -1,17 +1,20 @@
 { config, pkgs, ... }:
 
 {
+  # Root 分割槽
   fileSystems."/".device = "/dev/vda1";
   fileSystems."/".fsType = "ext4";
 
+  # swap
   swapDevices = [
     { device = "/dev/vda2"; }
   ];
 
+  # Bootloader（GRUB 安裝到 MBR）
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/vda";
-  boot.loader.efi.enable = false;
 
+  # 主機名稱
   networking.hostName = "laptop";
 }
